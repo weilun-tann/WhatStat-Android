1fcd78fb1eff3005db68427d7e9573029b1fce01
Wei Lun <weilunn.97@gmail.com>
2019-12-15 22:17:14 +0800

    Main Menu Re-design

--- app/src/main/java/com/jed/whatsapp/MainActivity.java
+++ app/src/main/java/com/jed/whatsapp/MainActivity.java
@@ -9,10 +9,11 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
@@ -22,13 +23,12 @@ import static com.jed.whatsapp.FileProcessing.*;
 import static com.jed.whatsapp.FileProcessing.setUploadedFile;
 
 // TODO : Add onClick to button in main_activity.xml
-public class MainActivity<UPLOAD_FILE_REQUEST_CODE> extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     // ATTRIBUTES
-    private ImageButton fileUploadButton;
-    private ImageButton viewMsgStatButton;
-    private static final int UPLOAD_FILE_REQUEST_CODE = 1;
-    private static final int VIEW_STATS_REQUEST_CODE = 2;
+    private static final int UPLOAD_REQUEST_CODE = 1;
+    private static final int ANALYZE_REQUEST_CODE = 2;
+    private static final int GRAPH_REQUEST_CODE = 3;
 
 
     @Override
@@ -36,7 +36,8 @@ public class MainActivity<UPLOAD_FILE_REQUEST_CODE> extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        // EMAIL BUTTON
+        FloatingActionButton fab = findViewById(R.id.emailButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,23 +46,34 @@ public class MainActivity<UPLOAD_FILE_REQUEST_CODE> extends AppCompatActivity {
             }
         });
 
-        fileUploadButton = findViewById(R.id.fileUploadButton);
-        fileUploadButton.setOnClickListener(new View.OnClickListener() {
+        // UPLOAD BUTTON
+        Button uploadButton = findViewById(R.id.uploadButton);
+        uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
                 intent.setType("text/plain");
-                startActivityForResult(intent, UPLOAD_FILE_REQUEST_CODE);
+                startActivityForResult(intent, UPLOAD_REQUEST_CODE);
             }
         });
 
-        viewMsgStatButton = findViewById(R.id.viewMsgStatButton);
-        viewMsgStatButton.setOnClickListener(new View.OnClickListener() {
+        // ANALYZE BUTTON
+        Button analyzeButton = findViewById(R.id.analyzeButton);
+        analyzeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MessageStatisticsActivity.class);
                 startActivityForResult(intent, 100);
+            }
+        });
+
+        // GRAPH BUTTON
+        Button graphButton = findViewById(R.id.graphButton);
+        graphButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : IMPLEMENT GRAPHING ONCLICK FUNCTION
             }
         });
     }
@@ -70,7 +82,7 @@ public class MainActivity<UPLOAD_FILE_REQUEST_CODE> extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (resultCode == RESULT_OK) {
             switch (requestCode) {
-                case UPLOAD_FILE_REQUEST_CODE:
+                case UPLOAD_REQUEST_CODE:
                     String selectedFilePath = data.getData().getPath();
                     File selectedFile = new File(selectedFilePath);
                     setUploadedFile(selectedFile);
@@ -79,10 +91,15 @@ public class MainActivity<UPLOAD_FILE_REQUEST_CODE> extends AppCompatActivity {
                         readFile(data.getData(), getApplicationContext());
                         FileProcessing.retrieveConversationHistory();
                     } catch (IOException e) {
-                        Toast.makeText(getApplicationContext(), "Upload failed. Supports .txt files only.",
-                                Toast.LENGTH_SHORT).show();
-                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(), "Mhm... are you sure that was a " +
+                                "WhatsApp conversation text file?", Toast.LENGTH_SHORT).show();
                     }
+                    break;
+
+                case ANALYZE_REQUEST_CODE:
+                    break;
+
+                case GRAPH_REQUEST_CODE:
                     break;
             }
         }
